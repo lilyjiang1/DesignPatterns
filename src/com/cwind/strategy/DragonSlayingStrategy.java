ad2c945a31df237e9fb1549ee74c279eec4120a5
lilyjiang1 <cwkof2005@126.com>
2015-07-12 18:13:49 +0800

    Strategy Pattern

---
+++ src/com/cwind/strategy/DragonSlayingStrategy.java
@@ -0,0 +1,13 @@
+package com.cwind.strategy;/**
+ * Created by Wen Chen on 2015/7/12.
+ */
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Cwind
+ * Date: 2015/7/12
+ * Time: 18:13
+ * Email: billchen01@163.com
+ */
+public class DragonSlayingStrategy {
+}
